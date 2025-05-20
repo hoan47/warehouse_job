911d4b682719bc42bf7e793b47c099f7efe939ba
VocabularyAPK <b2h1604bmt@gmail.com>
2025-05-20 23:36:00 +0700

    .

---
+++ staging_database/ETL_job_source_to_stage.sql
@@ -0,0 +1,130 @@
+USE job;
+GO
+
+-- 1. ETL cho bảng CompanyStage
+INSERT INTO jobDWStage.dbo.CompanyStage (CompanyId, CompanyName, CompanyLogo, Address)
+SELECT 
+    CompanyId,
+    CompanyName,
+    CompanyLogo,
+    Address
+FROM job.dbo.Company;
+
+-- 2. ETL cho bảng CityStage
+INSERT INTO jobDWStage.dbo.CityStage (CityId, CityName, CityNameVI)
+SELECT 
+    CityId,
+    CityName,
+    CityNameVI
+FROM job.dbo.City;
+
+-- 3. ETL cho bảng IndustryStage
+INSERT INTO jobDWStage.dbo.IndustryStage (IndustryId, IndustryName, IndustryNameVI)
+SELECT 
+    IndustryId,
+    IndustryName,
+    IndustryNameVI
+FROM job.dbo.Industry;
+
+-- 4. ETL cho bảng JobFunctionStage
+INSERT INTO jobDWStage.dbo.JobFunctionStage (JobFunctionId, JobFunctionName, JobFunctionNameVI)
+SELECT 
+    JobFunctionId,
+    JobFunctionName,
+    JobFunctionNameVI
+FROM job.dbo.JobFunction;
+
+-- 5. ETL cho bảng GroupJobFunctionStage
+INSERT INTO jobDWStage.dbo.GroupJobFunctionStage (GroupJobFunctionId, GroupJobFunctionName, GroupJobFunctionNameVI)
+SELECT 
+    GroupJobFunctionId,
+    GroupJobFunctionName,
+    GroupJobFunctionNameVI
+FROM job.dbo.GroupJobFunction;
+
+-- 6. ETL cho bảng SkillStage
+INSERT INTO jobDWStage.dbo.SkillStage (SkillId, SkillName)
+SELECT 
+    SkillId,
+    SkillName
+FROM job.dbo.Skill;
+
+-- 7. ETL cho bảng BenefitTypeStage
+INSERT INTO jobDWStage.dbo.BenefitTypeStage (BenefitId, BenefitName, BenefitNameVI, BenefitIconName)
+SELECT 
+    BenefitId,
+    BenefitName,
+    BenefitNameVI,
+    BenefitIconName
+FROM job.dbo.BenefitType;
+
+-- 8. ETL cho bảng LanguageStage
+INSERT INTO jobDWStage.dbo.LanguageStage (LanguageId, LanguageName, LanguageNameVI)
+SELECT 
+    LanguageId,
+    LanguageName,
+    LanguageNameVI
+FROM job.dbo.Language;
+
+-- 9. ETL cho bảng JobStage
+INSERT INTO jobDWStage.dbo.JobStage (
+    JobId, JobTitle, JobUrl, CompanyId, SalaryMin, SalaryMax, SalaryCurrency, 
+    JobLevel, TypeWorkingId, ApprovedOn, OnlineOn, ExpiredOn, IsUrgentJob, 
+    IsTopPriority, IsMobileHotJob, JobDescription, JobRequirement, LanguageId, 
+    JobFunctionId, GroupJobFunctionId
+)
+SELECT 
+    JobId,
+    JobTitle,
+    JobUrl,
+    CompanyId,
+    SalaryMin,
+    SalaryMax,
+    SalaryCurrency,
+    JobLevel,
+    TypeWorkingId,
+    ApprovedOn,
+    OnlineOn,
+    ExpiredOn,
+    IsUrgentJob,
+    IsTopPriority,
+    IsMobileHotJob,
+    JobDescription,
+    JobRequirement,
+    LanguageId,
+    JobFunctionId,
+    GroupJobFunctionId
+FROM job.dbo.Job;
+
+-- 10. ETL cho bảng WorkingLocationStage
+INSERT INTO jobDWStage.dbo.WorkingLocationStage (WorkingLocationId, JobId, Address, CityId, Latitude, Longitude)
+SELECT 
+    WorkingLocationId,
+    JobId,
+    Address,
+    CityId,
+    Latitude,
+    Longitude
+FROM job.dbo.WorkingLocation;
+
+-- 11. ETL cho bảng JobIndustryStage
+INSERT INTO jobDWStage.dbo.JobIndustryStage (JobId, IndustryId)
+SELECT 
+    JobId,
+    IndustryId
+FROM job.dbo.JobIndustry;
+
+-- 12. ETL cho bảng JobSkillStage
+INSERT INTO jobDWStage.dbo.JobSkillStage (JobId, SkillId)
+SELECT 
+    JobId,
+    SkillId
+FROM job.dbo.JobSkill;
+
+-- 13. ETL cho bảng JobBenefitStage
+INSERT INTO jobDWStage.dbo.JobBenefitStage (JobId, BenefitId, BenefitValue)
+SELECT 
+    JobId,
+    BenefitId,
+    BenefitValue
+FROM job.dbo.JobBenefit;
